27dea4eded1f97fe98c5c93678eac60b4ea0a030
Anderson Alves <andersoonalvess@gmail.com>
2020-04-19 17:52:25 -0300

    [NodeJS] Create models and migrations

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,17 +1,15 @@
-import { uuid } from 'uuidv4';
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
+@Entity('appoitntments')
 class Appointment {
+    @PrimaryGeneratedColumn('uuid')
     id: string;
 
+    @Column()
     provider: string;
 
+    @Column('timestamp with time zone')
     date: Date;
-
-    constructor({ provider, date }: Omit<Appointment, 'id'>) {
-        this.id = uuid();
-        this.provider = provider;
-        this.date = date;
-    }
 }
 
 export default Appointment;
