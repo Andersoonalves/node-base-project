7dd217f81a492e31da3f34775e2c12bd1ef5db42
Anderson Alves <andersoonalvess@gmail.com>
2020-04-19 18:52:47 -0300

    [NodeJs] Create User migration and Model!

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -2,7 +2,6 @@ import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppoitmentsRepository';
-import appointmentsRouter from '../routes/appointments.routes';
 
 interface RequestDTO {
     provider: string;
