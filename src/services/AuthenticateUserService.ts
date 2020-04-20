9909b25a911b50805f78a9ca0b18f123ea21190e
Anderson Alves <andersoonalvess@gmail.com>
2020-04-19 21:14:00 -0300

    [NodeJS] Create token with JWT

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 import User from '../models/User';
 
 interface RequestDTO {
@@ -9,6 +10,7 @@ interface RequestDTO {
 
 interface Response {
     user: User;
+    token: string;
 }
 
 class AuthenticateUserService {
@@ -27,7 +29,12 @@ class AuthenticateUserService {
             throw new Error('Incorrect email password combination.');
         }
 
-        return { user };
+        const token = sign({}, 'cfcea5b40df9cc9067f22189b92c07dd', {
+            subject: user.id,
+            expiresIn: '1d',
+        });
+
+        return { user, token };
     }
 }
 
